31b3c7ca676e9f4dda63868337f3cced655340ce
pehcastro <pehcastro@gmail.com>
2024-07-24 23:03:04 -0300

    chore: first upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="flex flex-col justify-center mt-52">
+	<h1 class="flex justify-center">mgpai pool</h1>
+	<div class="flex justify-center">
+		<Button>yay</Button>
+	</div>
+</div>
